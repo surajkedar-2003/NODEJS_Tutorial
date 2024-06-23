3bd3ff13db29bdd51648128a462582e5eafc5147
SurajKedar-2003 <surajkedar658@gmail.com>
2024-06-23 18:26:44 +0530

    learn file module

---
+++ filesModule/file2.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+var data = fs.readFileSync('/f1.txt', 'utf-8');
+console.log(data);
---
+++ filesModule/path.js
@@ -0,0 +1,4 @@
+console.log(__dirname);
+const path = require('path');
+const txtpath = path.join(__dirname, 'file');
+console.log(txtpath);
